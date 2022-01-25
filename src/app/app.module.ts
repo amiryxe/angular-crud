d25698e6df74047ded60d57df02420e7afc290ba
Amir Salehi <nik.amirsa@gmail.com>
2022-01-25 10:09:04 +0330

    create task list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
